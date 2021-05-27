70ce00396e2a4ce58a8666fa675e1d374b69de72
star-39 <star3nine@mail.ru>
2021-05-28 02:07:16 +0900

    Improve buildah script

--- buildah_fedora_moe_sticker_bot.sh
+++ buildah_fedora_moe_sticker_bot.sh
@@ -9,15 +9,15 @@ c1=$(buildah from fedora:34)
 # prepare repos
 buildah run $c1 -- dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-34.noarch.rpm -y
 
-# install dependencies
+# install system dependencies
 buildah run $c1 -- dnf install python3.9 python-pip bsdtar ImageMagick ffmpeg libwebp curl -y
-buildah run $c1 -- pip3 install wheel python-telegram-bot requests beautifulsoup4 emoji lottie[GIF]
-
-buildah run $c1 -- dnf clean all
 
 # grab sources
 buildah run $c1 -- curl -Lo /moe-sticker-bot.zip https://github.com/star-39/moe-sticker-bot/archive/refs/heads/master.zip 
 buildah run $c1 -- bsdtar -xvf /moe-sticker-bot.zip -C /
+
+# install python dependencies
+buildah run $c1 -- pip3 install -r /moe-sticker-bot-master/requirements.txt
 
 # finish
 buildah config --cmd '' $c1
@@ -26,11 +26,13 @@ buildah config --entrypoint "cd /moe-sticker-bot-master && /usr/bin/python3 main
 # Fix python3.8+'s problem.
 buildah config --env COLUMNS=80 $c1
 
+# clean up
+buildah run $c1 -- dnf autoremove python3-pip
+buildah run $c1 -- dnf clean all
+
 buildah commit $c1 moe-sticker-bot
 
 buildah login -u star-39 -p $GITHUB_TOKEN ghcr.io
 
 buildah push moe-sticker-bot ghcr.io/star-39/moe-sticker-bot:latest
-
-buildah rm $c1
 
