d0eac657d1125ae2e11793f8525b600cf5def26b
star-39 <star3nine@mail.ru>
2022-02-08 16:02:30 +0000

    Fix imports

--- main.py
+++ main.py
@@ -12,9 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from cProfile import run
 import json
-from operator import is_
 import time
 # import logging
 from typing import Any
