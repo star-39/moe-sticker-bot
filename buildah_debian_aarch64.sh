c8a8394172da6cc37d53c696ab6198a7b62bdfcf
star-39 <75669297+star-39@users.noreply.github.com>
2022-03-02 17:34:29 +0900

    Update buildah_debian_aarch64.sh

--- buildah_debian_aarch64.sh
+++ buildah_debian_aarch64.sh
@@ -6,30 +6,30 @@ buildah login -u star-39 -p $GITHUB_TOKEN ghcr.io
 
 #################################
 
-# c1=$(buildah from arm64v8/debian:sid)
+c1=$(buildah from arm64v8/debian:sid)
 
-# buildah run $c1 -- apt update -y
-# buildah run $c1 -- apt install ffmpeg python3 python3-pip imagemagick curl libarchive-tools libmariadb-dev -y
+buildah run $c1 -- apt update -y
+buildah run $c1 -- apt install ffmpeg python3 python3-pip imagemagick curl libarchive-tools libmariadb-dev -y
 
-# buildah run $c1 -- curl -Lo /requirements.txt https://github.com/star-39/moe-sticker-bot/raw/master/requirements.txt
+buildah run $c1 -- curl -Lo /requirements.txt https://github.com/star-39/moe-sticker-bot/raw/master/requirements.txt
 
-# buildah run $c1 -- pip3 install wheel setuptools
-# buildah run $c1 -- pip3 install -r /requirements.txt
-# buildah run $c1 -- pip3 cache purge
+buildah run $c1 -- pip3 install wheel setuptools
+buildah run $c1 -- pip3 install -r /requirements.txt
+buildah run $c1 -- pip3 cache purge
 
-# buildah run $c1 -- apt autoremove python3-pip -y
-# buildah run $c1 -- apt install python3-setuptools -y
-# buildah run $c1 -- apt autoclean
+buildah run $c1 -- apt autoremove python3-pip -y
+buildah run $c1 -- apt install python3-setuptools -y
+buildah run $c1 -- apt autoclean
 
-# buildah config --cmd '' $c1
-# buildah config --entrypoint "cd /moe-sticker-bot-master && /usr/bin/python3 main.py" $c1
+buildah config --cmd '' $c1
+buildah config --entrypoint "cd /moe-sticker-bot-master && /usr/bin/python3 main.py" $c1
 
-# # Fix python3.8+'s problem.
-# buildah config --env COLUMNS=80 $c1
+# Fix python3.8+'s problem.
+buildah config --env COLUMNS=80 $c1
 
-# buildah commit $c1 moe-sticker-bot:base_aarch64
+buildah commit $c1 moe-sticker-bot:base_aarch64
 
-# buildah push moe-sticker-bot ghcr.io/star-39/moe-sticker-bot:base_aarch64
+buildah push moe-sticker-bot ghcr.io/star-39/moe-sticker-bot:base_aarch64
 
 #################################
 
