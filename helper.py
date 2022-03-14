5a26a7025b52fe578f1f99c18f669c57f69cc50a
star-39 <star3nine@mail.ru>
2022-03-14 00:43:00 +0000

    Revert "Prepare stickers immediately in separate process"
    This reverts commit 625598dd63245aa325bdd1f02310c9291731bef7.

--- helper.py
+++ helper.py
@@ -20,6 +20,10 @@ import re
 
 from macros import *
 
+# Names of binaries that we depend on vary across different OSes.
+# To make the code truely cross-platform, this problem should be sloved in code,
+# but not in environment.
+
 
 def get_ffmpeg_bin():
     b = ['ffmpeg']
@@ -228,7 +232,7 @@ def get_line_sticker_detail(message, ctx: CallbackContext):
 
 
 
-def prepare_sticker_files(update: Update, ctx: CallbackContext, q):
+def prepare_sticker_files(update: Update, ctx: CallbackContext):
     time_start = time.time()
     images = []
     # User stickers
@@ -329,8 +333,10 @@ def prepare_sticker_files(update: Update, ctx: CallbackContext, q):
 
                 images = sorted([f for f in glob.glob(
                     os.path.join(work_dir, "**", "*.webm"), recursive=True)])
-    #debug
-    q.put(images)
+
+    if len(images) == 0:
+        raise Exception("No image available! Try again.")
+    ctx.user_data['telegram_sticker_files'] = images
 
     time_end = time.time()
     print(
@@ -393,8 +399,6 @@ def initialize_user_data(in_command, update: Update, ctx):
     clean_userdata(update, ctx)
     ctx.user_data['in_command'] = in_command
     ctx.user_data['manual_emoji'] = False
-    ctx.user_data['line_process'] = None
-    ctx.user_data['line_queue'] = None
     ctx.user_data['line_sticker_url'] = ""
     ctx.user_data['line_store_webpage'] = None
     ctx.user_data['line_sticker_download_url'] = ""
@@ -416,9 +420,6 @@ def initialize_user_data(in_command, update: Update, ctx):
 
 # Clean temparary user data after each conversasion.
 def clean_userdata(update: Update, ctx: CallbackContext):
-    if 'line_process' in ctx.user_data:
-        if ctx.user_data['line_process'] is not None:
-            ctx.user_data['line_process'].kill()
     ctx.user_data.clear()
     userdata_dir = os.path.join(main.DATA_DIR, str(update.effective_user.id))
     if os.path.exists(userdata_dir):
