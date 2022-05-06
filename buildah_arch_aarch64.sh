94621d688d6e0142080c295ef39a83f66c629c88
star-39 <star3nine@mail.ru>
2022-05-06 15:16:54 +0000

    Update GHA

--- buildah_arch_aarch64.sh
+++ buildah_arch_aarch64.sh
@@ -6,23 +6,23 @@ buildah login -u star-39 -p $GITHUB_TOKEN ghcr.io
 
 #################################
 
-c1=$(buildah from docker://arm64v8/archlinux:latest)
+# c1=$(buildah from docker://lopsided/archlinux-arm64v8:latest)
 
-buildah run $c1 -- pacman -Sy
-buildah run $c1 -- pacman --noconfirm -S libheif ffmpeg imagemagick curl libarchive 
-buildah run $c1 -- sh -c 'yes | pacman -Scc'
+# buildah run $c1 -- pacman -Sy
+# buildah run $c1 -- pacman --noconfirm -S libheif ffmpeg imagemagick curl libarchive 
+# buildah run $c1 -- sh -c 'yes | pacman -Scc'
 
-buildah config --cmd '/moe-sticker-bot' $c1
+# buildah config --cmd '/moe-sticker-bot' $c1
 
-buildah commit $c1 moe-sticker-bot:base
+# buildah commit $c1 moe-sticker-bot:base
 
-buildah push moe-sticker-bot ghcr.io/star-39/moe-sticker-bot:base
+# buildah push moe-sticker-bot ghcr.io/star-39/moe-sticker-bot:base
 
 #################################
 
 c1=$(buildah from ghcr.io/star-39/moe-sticker-bot:base_aarch64)
 
-go build
+GOOS=linux GOARCH=arm64 go build
 buildah copy $c1 moe-sticker-bot /moe-sticker-bot
 
 buildah commit $c1 moe-sticker-bot:latest
