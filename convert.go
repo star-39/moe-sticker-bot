ebda57f3352c3dad7e98d0f0f8a6cb907cf492e6
star-39 <star3nine@mail.ru>
2022-05-10 11:36:40 +0000

    Fix wrong libvpx param

--- convert.go
+++ convert.go
@@ -52,7 +52,7 @@ func ffToWebm(f string) (string, error) {
 		log.Warn("ff to webm too big, retrying...")
 		args = []string{"-hide_banner", "-i", f,
 			"-vf", "scale=512:512:force_original_aspect_ratio=decrease:flags=lanczos", "-pix_fmt", "yuva420p",
-			"-c:v", "libvpx-vp9", "-cpu-used", "6", "-minrate", "50k", "-b:v", "200k", "-maxrate", "300",
+			"-c:v", "libvpx-vp9", "-cpu-used", "6", "-minrate", "50k", "-b:v", "200k", "-maxrate", "300k",
 			"-to", "00:00:02.900", "-an", "-y", pathOut}
 		err = exec.Command(bin, args...).Run()
 	}
