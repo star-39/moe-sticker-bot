5890ad24ad55a95cdf93844d9cb01d70e6556bfa
star-39 <star3nine@mail.ru>
2022-05-12 04:33:43 +0000

    Fix rand number

--- import.go
+++ import.go
@@ -195,7 +195,7 @@ func prepLineStickers(ud *UserData, needConvert bool) error {
 	defer ud.udWg.Done()
 	ud.stickerData.isVideo = ud.lineData.isAnimated
 
-	ud.stickerData.id = "line_" + ud.lineData.id + secNum(2) + "_by_" + botName
+	ud.stickerData.id = "line_" + ud.lineData.id + secNum(4) + "_by_" + botName
 	log.Debugf("Generated ID:%s", ud.stickerData.id)
 	ud.stickerData.title = ud.lineData.title + " @" + botName
 
