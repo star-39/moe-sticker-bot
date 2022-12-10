e801268139b2898f60e6fff76fe54c059e8cc422
star-39 <star3nine@mail.ru>
2022-12-10 11:58:02 +0000

    Fix webapp image AR

--- web/webapp3/src/Sticker.js
+++ web/webapp3/src/Sticker.js
@@ -4,26 +4,36 @@ import Img from "react-cool-img";
 // import './StickerStyle.css'
 import loading_gif from './loading.gif'
 
+// const mediaStyle = {
+//   marginLeft: "auto",
+//   marginRight: "auto",
+//   display: "block"
+// }
+
 export const Sticker = forwardRef(({ id, faded, style, emoji, surl, onEmojiChange, ...props }, ref) => {
 
   if (surl.endsWith(".webp")) {
     return (
       <div ref={ref} style={style} {...props}>
+        {/* <div style={{width: 64, height: 64}}> */}
         <Img src={surl} placeholder={loading_gif} alt="Loading..."
           retry={{ count: 10, delay: 2, acc: false }}
-          width="64" height="64"
+          width="auto" height="64" max-width="64" 
         ></Img>
+        {/* </div> */}
         <br />
-        <label>{id}</label>
-        <input type="text" value={emoji} size="6"
-          onChange={(e) => onEmojiChange(id, e.target.value)}></input>
+        <div>
+          <label>{id}</label>
+          <input type="text" value={emoji} size="6"
+            onChange={(e) => onEmojiChange(id, e.target.value)}></input>
+        </div>
       </div>
     );
   } else {
     return (
       <div ref={ref} style={style} {...props}>
-        <video loop muted autoplay playsinline width="64">
-          <source src={surl} type="video/webm"  />
+        <video loop muted autoplay playsinline width="auto" height="64" max-width="64" >
+          <source src={surl} type="video/webm" />
         </video>
         <br />
         <label>{id}</label>
