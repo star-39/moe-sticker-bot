78b3135bd404f7851eb4442d7ca1b332284f190a
star-39 <star3nine@mail.ru>
2023-02-09 10:52:59 +0000

    Improved msbimport cmd

--- cmd/msbimport/main.go
+++ cmd/msbimport/main.go
@@ -6,15 +6,17 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
+	"github.com/star-39/moe-sticker-bot/pkg/convert"
 	"github.com/star-39/moe-sticker-bot/pkg/msbimport"
 )
 
 func main() {
 	var link = flag.String("link", "", "Import link(LINE/kakao)")
-	var convert = flag.Bool("convert", false, "Convert to Telegram format(WEBP/WEBM)")
+	var convertTG = flag.Bool("convert", false, "Convert to Telegram format(WEBP/WEBM)")
 	flag.Parse()
 
 	log.SetLevel(log.DebugLevel)
+	convert.InitConvert()
 
 	ctx, _ := context.WithCancel(context.Background())
 	ld := &msbimport.LineData{}
@@ -24,27 +26,22 @@ func main() {
 	if err != nil {
 		log.Error("Error parsing import link!")
 		log.Fatalln(err)
-		//handle error here.
 	}
 	if warn != "" {
 		log.Warnln(warn)
-		//handle warning message here.
 	}
 
-	err = msbimport.PrepareImportStickers(ctx, ld, "./", *convert)
+	err = msbimport.PrepareImportStickers(ctx, ld, "./", *convertTG)
 	if err != nil {
 		log.Fatalln(err)
-		//handle error here.
 	}
 
 	for _, lf := range ld.Files {
 		lf.Wg.Wait()
 		if lf.CError != nil {
 			log.Error(lf.CError)
-			//hanlde sticker error here.
 		}
 		println(lf.OriginalFile)
 		println(lf.ConvertedFile)
-		//...
 	}
 }
