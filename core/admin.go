8ef66de1ad8faa8a6df769bd1aa9f5e53f575ead
star-39 <star3nine@mail.ru>
2023-02-13 15:46:47 +0000

    Improved admin

--- core/admin.go
+++ core/admin.go
@@ -1,17 +1,15 @@
 package core
 
 import (
-	"strings"
-
 	tele "gopkg.in/telebot.v3"
 )
 
 func cmdSitRep(c tele.Context) error {
 	// Report status.
-	stat := []string{}
-	py_emoji_ok, _ := httpGet("http://127.0.0.1:5000/status")
-	stat = append(stat, "py_emoji_ok? :"+py_emoji_ok)
-	c.Send(strings.Join(stat, "\n"))
+	// stat := []string{}
+	// py_emoji_ok, _ := httpGet("http://127.0.0.1:5000/status")
+	// stat = append(stat, "py_emoji_ok? :"+py_emoji_ok)
+	// c.Send(strings.Join(stat, "\n"))
 
 	return nil
 }
