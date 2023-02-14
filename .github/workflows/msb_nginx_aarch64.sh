3c3aefbaaeda119e5e5b441d0106cff2e1a4fd80
star-39 <star3nine@mail.ru>
2023-02-14 05:08:44 +0000

    Fix AArch64 nginx workflow

--- .github/workflows/msb_nginx_aarch64.sh
+++ .github/workflows/msb_nginx_aarch64.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
-
 GITHUB_TOKEN=$1
 
 buildah login -u star-39 -p $GITHUB_TOKEN ghcr.io
-
 
 #AArch64
 c1=$(buildah from docker://arm64v8/nginx:latest)
@@ -12,9 +10,12 @@ c1=$(buildah from docker://arm64v8/nginx:latest)
 buildah run  $c1 -- mkdir -p /etc/nginx/templates
 buildah copy $c1 web/nginx/default.conf.template /etc/nginx/templates/
 
-# Copy react app
+# Build react app
 cd web/webapp3/
+npm install
+PUBLIC_URL=/webapp REACT_APP_HOST=msb.cloudns.asia npm run build
 buildah copy $c1 build/ /webapp
+cd ../..
 
 buildah commit $c1 moe-sticker-bot:msb_nginx_aarch64
 
